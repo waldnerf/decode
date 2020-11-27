2147343a13c5987ced11fe3987cce93152f8d553
Waldner <francois.waldner@gmail.com>
2020-11-27 11:56:14 +0100

    add: structure + ideas

---
+++ postprocessing/instance_segmentation.py
@@ -0,0 +1,45 @@
+import higra as hg
+import numpy as np
+
+def InstSegm(extent, boundary, t_ext=0.4, t_bound=0.2):
+    """
+    INPUTS:
+        extent   : extent prediction
+        boundary : boundary prediction
+        t_ext    : threshold for extent
+        t_bound  : threshold for boundary
+    OUTPUT:
+        instances
+    """
+    
+    # Threshold extent mask
+    ext_binary = np.uint8(extent>=t_ext)
+
+    # Artificially create strong boundaries for 
+    # pixels with non-field labels
+    input_hws = np.copy(boundary)
+    input_hws[extent_bin==0]=1
+    
+    # Create the directed graph
+    size = input_hws.shape[:2]
+    graph = hg.get_8_adjacency_graph(size)
+    edge_weights = hg.weight_graph(
+           graph,
+           input_hws,
+           hg.WeightFunction.mean
+        )
+
+    tree,altitudes=hg.watershed_hierarchy_by_dynamics(
+           graph,
+           edge_weights
+        )
+    # Get individual fields 
+    # by cutting the graph using altitude
+    instances = 
+    hg.labelisation_horizontal_cut_from_threshold(
+           tree,
+           altitudes,
+           threshold=t_boundary
+        )
+
+    return instances
